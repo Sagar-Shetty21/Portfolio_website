f89f5ab789b80493cd7efb6f639f9980d27ebcf7
sagar-shetty21 <5agarm5hetty05@gmail.com>
2023-11-04 22:15:31 +0530

    css changes

--- Scripts/DarkLightMode.js
+++ Scripts/DarkLightMode.js
@@ -25,8 +25,7 @@ function changeMode(){
 
 function toDarkMode() {
     var root = document.querySelector(':root');
-    root.style.setProperty('--backgroundColor','radial-gradient(ellipse at bottom, #1B2735 0%, #090A0F 100%)');
-    root.style.setProperty('--bg-stars-color','#FFF');
+    
 
     root.style.setProperty('--navbar-bg','#202230');
     root.style.setProperty('--navbar-shadow','rgb(99, 74, 116)');
@@ -79,6 +78,68 @@ function toDarkMode() {
     root.style.setProperty('--contact-form-submit-bg','#08d');
     root.style.setProperty('--contact-form-submit-text','#eee');
     root.style.setProperty('--contact-form-submit-active-bg','rgb(3, 14, 68)');
+
+
+
+
+    root.style.setProperty('--backgroundColor','radial-gradient(ellipse at bottom, #1B2735 0%, #090A0F 100%)');
+    root.style.setProperty('--bg-stars-color','#FFF');
+    root.style.setProperty('--toggle-bg','#82cac3');
+    root.style.setProperty('--toggle-ball-bg','rgb(117, 214, 177)');
+    root.style.setProperty('--toggle-shadow','0 0 20px rgba(26, 25, 25, 0.781)');
+    
+    root.style.setProperty('--navbar-bg','#000000a4');
+    root.style.setProperty('--navbar-shadow','#FF6701');
+    root.style.setProperty('--navbar-text-color','white');
+    root.style.setProperty('--navitem-hover','#FF6701');
+    root.style.setProperty('--navitem-line','#009378');
+    //home section
+    root.style.setProperty('--intro-small','#c2c2c2');
+    root.style.setProperty('--intro-big','#FFFFFF');
+    root.style.setProperty('--intro-big-name','#FF6701');
+    root.style.setProperty('--intro-bigger-1','#FFFFFF');
+    root.style.setProperty('--intro-bigger-2','#FFFFFF');
+    root.style.setProperty('--intro-bigger-3','#FFFFFF');
+    root.style.setProperty('--social-button-bg','#C53678');
+    root.style.setProperty('--social-button','#ffffffb7');
+    root.style.setProperty('--social-button-hover','#ffffff');
+    root.style.setProperty('--intro-button-bg','#FF6701');
+    root.style.setProperty('--intro-button-bg-hover','#FEA82F');
+    root.style.setProperty('--intro-button-bg-hover-shadow','#FF6701');
+    //about section
+    root.style.setProperty('--about-card-bg','transparent');
+    root.style.setProperty('--about-card-text','black');
+    root.style.setProperty('--about-heading-color','#FF6701');
+    root.style.setProperty('--about-card-details-bg','#1EE494');
+    root.style.setProperty('--about-card-details-button-bg','inherit');
+    root.style.setProperty('--about-card-details-button-bg-hover','#FF6701');
+    root.style.setProperty('--about-card-skills-border','#1EE494');
+    root.style.setProperty('--about-card-skills-button-bg','#1EE494');
+    //work section
+    root.style.setProperty('--work-card-bg','transparent');
+    root.style.setProperty('--work-title-color1','#FF6701');
+    root.style.setProperty('--work-title-shadow','1px 0px 4px #FF6701, 2px 0px 4px #f07a2c, 3px 0px 4px #FF6701, 2px 0px 3px #FF6701, 2px 3px 15px #c56e34, 2px 0px 15px, 5px 0px 125px, 20px 0vw 200px #fc8739,40px 0vw 200px #cc8353');
+    root.style.setProperty('--proj-cont-bg','#f6f6f6a6');
+    root.style.setProperty('--proj-cont-bg-hover','#F6F6F6');
+    root.style.setProperty('--proj-cont-hover-shadow','rgba(33, 33, 33, 0.841)');
+    root.style.setProperty('--work-card-border-color','#444444');
+    root.style.setProperty('--proj-cont-inner','inherit');
+    root.style.setProperty('--proj-title','#009378');
+    root.style.setProperty('--proj-title-line','#FF6701');
+    root.style.setProperty('--proj-stack','#C53678');
+    root.style.setProperty('--proj-detail','#023047');
+    root.style.setProperty('--proj-btn','#ff6701cb');
+    //contact form
+    root.style.setProperty('--contact-card bg','transparent');
+    root.style.setProperty('--contact-form-bg','#57a597');
+    root.style.setProperty('--contact-form-title','#eeeeee');
+    root.style.setProperty('--contact-form-input-bg','#f6f6f6f1');
+    root.style.setProperty('--contact-form-input-text','#000000');
+    root.style.setProperty('--contact-form-input-placeholder','#65657b');
+    root.style.setProperty('--contact-form-input-placeholder-focus','#FF6701');
+    root.style.setProperty('--contact-form-submit-bg','#FF6701');
+    root.style.setProperty('--contact-form-submit-text','#ffffff');
+    root.style.setProperty('--contact-form-submit-active-bg','rgb(255, 72, 0)');
 }
 
 
@@ -92,53 +153,56 @@ function toLightMode() {
     
     root.style.setProperty('--navbar-bg','#F6F6F6');
     root.style.setProperty('--navbar-shadow','#FF6701');
+    root.style.setProperty('--navbar-text-color','black');
     root.style.setProperty('--navitem-hover','#FF6701');
     root.style.setProperty('--navitem-line','#009378');
     //home section
-    root.style.setProperty('--intro-small','#748DA6');
-    root.style.setProperty('--intro-big','#B9E9FC');
-    root.style.setProperty('--intro-big-name','#3A98B9');
-    root.style.setProperty('--intro-bigger-1','#90dbf4');
-    root.style.setProperty('--intro-bigger-2','#8eecf5');
-    root.style.setProperty('--intro-bigger-3','#98f5e1');
-    root.style.setProperty('--social-button-bg','#a6cbd3');
-    root.style.setProperty('--social-button','#231942b7');
-    root.style.setProperty('--social-button-hover','#231942');
-    root.style.setProperty('--intro-button-bg','#0e00003b');
-    root.style.setProperty('--intro-button-bg-hover','transparent');
-    root.style.setProperty('--intro-button-bg-hover-shadow','#c0d8eb');
+    root.style.setProperty('--intro-small','#222222d2');
+    root.style.setProperty('--intro-big','#222222');
+    root.style.setProperty('--intro-big-name','#FF6701');
+    root.style.setProperty('--intro-bigger-1','#222222');
+    root.style.setProperty('--intro-bigger-2','#222222');
+    root.style.setProperty('--intro-bigger-3','#222222');
+    root.style.setProperty('--social-button-bg','#C53678');
+    root.style.setProperty('--social-button','#ffffffb7');
+    root.style.setProperty('--social-button-hover','#ffffff');
+    root.style.setProperty('--intro-button-bg','#FF6701');
+    root.style.setProperty('--intro-button-bg-hover','#FEA82F');
+    root.style.setProperty('--intro-button-bg-hover-shadow','#FF6701');
     //about section
     root.style.setProperty('--about-card-bg','transparent');
     root.style.setProperty('--about-card-text','black');
-    root.style.setProperty('--about-card-details-bg','rgb(152, 246, 182)');
+    root.style.setProperty('--about-heading-color','#FF6701');
+    root.style.setProperty('--about-card-details-bg','#1EE494');
     root.style.setProperty('--about-card-details-button-bg','inherit');
-    root.style.setProperty('--about-card-details-button-bg-hover','#27d430');
-    root.style.setProperty('--about-card-skills-bg','rgba(171, 243, 242, 0.755)');
-    root.style.setProperty('--about-card-skills-button-bg','inherit');
+    root.style.setProperty('--about-card-details-button-bg-hover','#FF6701');
+    root.style.setProperty('--about-card-skills-border','#1EE494');
+    root.style.setProperty('--about-card-skills-button-bg','#1EE494');
     //work section
-    root.style.setProperty('--work-card-bg','#6f6fdd73');
-    root.style.setProperty('--work-title-color1','#8fcaee');
-    root.style.setProperty('--work-title-color2','#94f1c2');
-    root.style.setProperty('--proj-cont-bg','#6f6fdde2');
-    root.style.setProperty('--proj-cont-bg-hover','#6f6fdd');
+    root.style.setProperty('--work-card-bg','transparent');
+    root.style.setProperty('--work-title-color1','#FF6701');
+    root.style.setProperty('--work-title-shadow','1px 0px 4px #FF6701, 2px 0px 4px #f07a2c, 3px 0px 4px #FF6701, 2px 0px 3px #FF6701, 2px 3px 15px #c56e34, 2px 0px 15px, 5px 0px 125px, 20px 0vw 200px #fc8739,40px 0vw 200px #cc8353');
+    root.style.setProperty('--proj-cont-bg','#f6f6f6a6');
+    root.style.setProperty('--proj-cont-bg-hover','#F6F6F6');
     root.style.setProperty('--proj-cont-hover-shadow','rgba(33, 33, 33, 0.841)');
-    root.style.setProperty('--proj-cont-inner','rgb(134, 134, 234)');
-    root.style.setProperty('--proj-title','rgb(15, 29, 63)');
-    root.style.setProperty('--proj-title-line','rgb(158, 237, 211)');
-    root.style.setProperty('--proj-stack','rgba(79, 5, 101, 0.801)');
+    root.style.setProperty('--work-card-border-color','#444444');
+    root.style.setProperty('--proj-cont-inner','inherit');
+    root.style.setProperty('--proj-title','#009378');
+    root.style.setProperty('--proj-title-line','#FF6701');
+    root.style.setProperty('--proj-stack','#C53678');
     root.style.setProperty('--proj-detail','#023047');
-    root.style.setProperty('--proj-btn','rgba(73, 126, 186, 0.842)');
+    root.style.setProperty('--proj-btn','#ff6701cb');
     //contact form
     root.style.setProperty('--contact-card bg','transparent');
-    root.style.setProperty('--contact-form-bg','#15172b');
-    root.style.setProperty('--contact-form-title','#eee');
-    root.style.setProperty('--contact-form-input-bg','#303245');
-    root.style.setProperty('--contact-form-input-text','#eee');
+    root.style.setProperty('--contact-form-bg','#57a597');
+    root.style.setProperty('--contact-form-title','#eeeeee');
+    root.style.setProperty('--contact-form-input-bg','#f6f6f6f1');
+    root.style.setProperty('--contact-form-input-text','#000000');
     root.style.setProperty('--contact-form-input-placeholder','#65657b');
-    root.style.setProperty('--contact-form-input-placeholder-focus','#dc2f55');
-    root.style.setProperty('--contact-form-submit-bg','#08d');
-    root.style.setProperty('--contact-form-submit-text','#eee');
-    root.style.setProperty('--contact-form-submit-active-bg','rgb(3, 14, 68)');
+    root.style.setProperty('--contact-form-input-placeholder-focus','#FF6701');
+    root.style.setProperty('--contact-form-submit-bg','#FF6701');
+    root.style.setProperty('--contact-form-submit-text','#ffffff');
+    root.style.setProperty('--contact-form-submit-active-bg','rgb(255, 72, 0)');
 }
 
 
